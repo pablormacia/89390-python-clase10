3bc292022db95fafd2a14fa21ef33666daa9e350
pablormacia <pr.macia@gmail.com>
2026-06-09 12:58:16 -0300

    Definición de modelos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField()
+
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE
+    )
+
+    tags = models.ManyToManyField(
+        Tag
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-published_date']
+
+    def clean(self):
+        if len(self.title) < 5:
+            raise ValidationError(
+                {'title': 'El título debe tener al menos 5 caracteres.'}
+            )
