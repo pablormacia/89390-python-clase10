46f5a65e8557fa59ec72287f89c5daf2bff11828
pablormacia <pr.macia@gmail.com>
2026-06-09 13:03:40 -0300

    Migraciones y administración

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Author, Post, Tag
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "email")
+    search_fields = ("name", "email")
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "published_date",
+    )
+
+    search_fields = (
+        "title",
+        "content",
+    )
+
+    list_filter = (
+        "author",
+        "published_date",
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
